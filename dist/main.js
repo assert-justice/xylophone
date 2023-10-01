2040da33af0540f4c3e95ecf658c4de18b99174d
riley <rileywestonmiller@outlook.com>
2023-09-30 21:03:12 -0700

    drawing walls

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,7 @@
 import { Game, System, Input, Window, Graphics } from 'cleo';
 import { Player } from './player.js';
 import { Sprite } from './sprite.js';
+import { Room } from './room.js';
 const {Texture} = Graphics;
 const tileWidth = 16;
 const roomWidth = 18;
@@ -12,32 +13,35 @@ let floorTile;
 let frameBuffer;
 let wallTile;
 let bg0, bg1;
+let room;
 
 Game.init = ()=>{
     player = new Player();
     player.spr = Texture.fromFile('./sprites/SpriteSheet.png');
-    floorTile = new Sprite();
-    floorTile.setProps({
-        spr: Texture.fromFile('./sprites/TilesetInteriorFloor.png'),
-        width:16,
-        height:16,
-        sx:16*14,
-        sy:16*15,
-        sw:16,
-        sh:16,
-    });
-    wallTile = new Sprite();
-    wallTile.setProps({
-        spr: Texture.fromFile('./sprites/TilesetWallSimple.png'),
-        width:16,
-        height:16,
-        // sx:16*19,
-        // sy:16*16,
-        sw:16,
-        sh:16,
-    });
+    // floorTile = new Sprite();
+    // floorTile.setProps({
+    //     spr: Texture.fromFile('./sprites/TilesetInteriorFloor.png'),
+    //     width:16,
+    //     height:16,
+    //     sx:16*14,
+    //     sy:16*15,
+    //     sw:16,
+    //     sh:16,
+    // });
+    // wallTile = new Sprite();
+    // wallTile.setProps({
+    //     spr: Texture.fromFile('./sprites/TilesetWallSimple.png'),
+    //     width:16,
+    //     height:16,
+    //     // sx:16*19,
+    //     // sy:16*16,
+    //     sw:16,
+    //     sh:16,
+    // });
     bg0 = Texture.new(roomWidth*tileWidth, roomHeight*tileWidth);
-    drawBackground(bg0);
+    room = new Room(bg0, roomWidth,roomHeight,tileWidth);
+    room.draw();
+    // drawBackground(bg0);
     frameBuffer = new Sprite();
     frameBuffer.setProps({
         spr: Texture.new(roomWidth*tileWidth, roomHeight*tileWidth),
@@ -58,19 +62,18 @@ Game.update = (dt)=>{
 Game.draw = ()=>{
     frameBuffer.spr.setTarget();
     Graphics.clear();
-    // drawBackground();
     bg0.draw(0,0);
     player.draw();
     frameBuffer.spr.resetTarget();
     frameBuffer.draw(0,0);
 }
 
-function drawBackground(bg){
-    bg.setTarget();
-    for(let x = 0; x < bg.width; x+=floorTile.width){    
-        for(let y = 0; y < bg.height; y+=floorTile.height){
-            floorTile.draw(x,y);
-        }
-    }
-    bg.resetTarget();
-}
+// function drawBackground(bg){
+//     bg.setTarget();
+//     for(let x = 0; x < bg.width; x+=floorTile.width){    
+//         for(let y = 0; y < bg.height; y+=floorTile.height){
+//             floorTile.draw(x,y);
+//         }
+//     }
+//     bg.resetTarget();
+// }
--- dist/room.js
+++ dist/room.js
@@ -1,5 +1,75 @@
-
+import { Graphics } from 'cleo';
+const {Texture} = Graphics;
+import { Sprite } from "./sprite.js";
 
 export class Room{
     bg;
+    floorTile;
+    wallTile;
+    width;
+    height;
+    tileWidth;
+    wallCoords = {
+        ulc:[0,6],
+        urc:[4,6],
+        dlc:[0,10],
+        drc:[4,10],
+        l:[0,7],
+        r:[4,7],
+        u:[3,6],
+        d:[3,10],
+    }
+    constructor(bg, width, height, tileWidth){
+        this.bg = bg; this.width = width;
+        this.height = height; this.tileWidth = tileWidth;
+        this.floorTile = new Sprite();
+        this.floorTile.setProps({
+            spr: Texture.fromFile('./sprites/TilesetInteriorFloor.png'),
+            width:tileWidth,
+            height:tileWidth,
+            sx:tileWidth*14,
+            sy:tileWidth*15,
+            sw:tileWidth,
+            sh:tileWidth,
+        });
+        this.wallTile = new Sprite();
+        this.wallTile.setProps({
+            spr: Texture.fromFile('./sprites/TilesetWallSimple.png'),
+            width:tileWidth,
+            height:tileWidth,
+            sw:tileWidth,
+            sh:tileWidth,
+        });
+    }
+    _drawWallTile([sx,sy],x,y){
+        sx*=this.tileWidth;
+        sy*=this.tileWidth;
+        if(x<0)x+=this.width;
+        if(y<0)y+=this.height;
+        x*=this.tileWidth;
+        y*=this.tileWidth;
+        this.wallTile.setProps({sx,sy});
+        this.wallTile.draw(x,y);
+    }
+    draw(){
+        this.bg.setTarget();
+        for(let x = 0; x < this.width; x++){    
+            for(let y = 0; y < this.height; y++){
+                this.floorTile.draw(x*this.tileWidth,y*this.tileWidth);
+            }
+        }
+        for(let x = 1; x < this.width-1; x++){
+            this._drawWallTile(this.wallCoords.u, x,0);
+            this._drawWallTile(this.wallCoords.d, x,this.height-1);
+        }
+        for(let y = 1; y < this.height-1; y++){
+            this._drawWallTile(this.wallCoords.l, 0,y);
+            this._drawWallTile(this.wallCoords.r, this.width-1,y);
+        }
+        this._drawWallTile(this.wallCoords.ulc,0,0);
+        this._drawWallTile(this.wallCoords.urc,-1,0);
+        this._drawWallTile(this.wallCoords.dlc,0,-1);
+        this._drawWallTile(this.wallCoords.drc,-1,-1);
+        this.bg.resetTarget();
+    }
 }
