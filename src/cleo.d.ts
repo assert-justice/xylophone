053ef212169112119a9b19f849534b2dfbec2526
Riley <waskerdu@gmail.com>
2023-10-01 22:24:38 -0700

    finish ts bindings

--- src/cleo.d.ts
+++ src/cleo.d.ts
@@ -1,7 +1,52 @@
 declare module "cleo" {
-    export class System {
-        static println(...args:any[]):void;
-        static readFile(path:string):string;
-        static writeFile(path:string,text:string):void;
+    export namespace System {
+        function println(...args:any[]):void;
+        function readFile(path:string):string;
+        function writeFile(path:string,text:string):void;
+    }
+    export class Game{
+        static init: ()=>{};
+        static update: (dt:number)=>{};
+        static draw: ()=>{};
+        static quit():void;
+    }
+    export namespace Graphics{
+        function setClearColor(r: number, g: number, b: number, a:number);
+        function clear():void;
+        class Texture{
+            width:number;
+            height:number;
+            draw(x:number, y:number): void;
+            static fromFile(path: string): Texture;
+            static new(width: number, height: number): Texture;
+            setTarget():void;
+            resetTarget():void;
+        }
+    }
+    export class Window{
+        static get width():number;
+        static get height():number;
+        static setStats(name: string, width:number, height:number, mode: string, monitor: number)
+        static vsync: boolean;
+
+    }
+    export class Input{
+        static keyIsDown(keyCode: number): boolean
+        static mouseButtonIsDown(mouseButtonCode: number): boolean
+        static get mouseX(): number
+        static get mouseY(): number
+        static joyButtonIsDown(joyIdx: number, buttonCode: number): boolean
+        static joyGetAxis(joyIdx: number, axisCode: number): boolean
+    }
+    export namespace Audio{
+        class Sound{
+            static fromFile(path: string)
+            play()
+            pause()
+            stop()
+            get isPlaying(): boolean
+            volume: number
+            isLooping: boolean
+        }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { System } from 'cleo';
+import { System, Game, Window } from 'cleo';
 
-System.println("yo", "sup");
+System.println("yo", "sup", Window.width);
 // System.writeFile()
