806aabd23e81292bb6bada391674bed7b96c6bc9
Riley <waskerdu@gmail.com>
2023-10-03 01:51:32 -0700

    more cleanup, remove compiled js

--- dist/constants.js
+++
@@ -1,3 +0,0 @@
-export const tileWidth = 16;
-export const roomWidth = 26;
-export const roomHeight = 18;
--- dist/game_state.js
+++
@@ -1,9 +0,0 @@
-import { tileWidth } from "./constants";
-export class GameState {
-    static toCoord(x, y) {
-        return [
-            Math.trunc(x / tileWidth),
-            Math.trunc(y / tileWidth),
-        ];
-    }
-}
